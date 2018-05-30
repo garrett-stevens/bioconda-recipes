8070926bf7e2d66c08aeb5142f9d039398687fca
Garrett Stevens <garrett.stevens@genomenext.com>
2018-05-30 10:30:24 -0400

    Clean up samtools build recipe

--- recipes/samtools/build.sh
+++ recipes/samtools/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sed -i.bak 's/^CPPFLAGS =$//g' Makefile
-sed -i.bak 's/^LDFLAGS  =$//g' Makefile
-
-sed -i.bak 's/^CPPFLAGS =$//g' htslib-$PKG_VERSION/Makefile
-sed -i.bak 's/^LDFLAGS  =$//g' htslib-$PKG_VERSION/Makefile
-
 # varfilter.py in install fails because we don't install Python
 sed -i.bak 's#misc/varfilter.py##g' Makefile
 
@@ -16,15 +10,13 @@ sed -i.bak 's/ -rdynamic//g' htslib-$PKG_VERSION/configure
 
 export CPPFLAGS="-DHAVE_LIBDEFLATE -I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
+export LIBS="-lcrypto -lcurl"
 
-cd htslib*
-./configure CPPFLAGS="$CPPFLAGS" --prefix=$PREFIX --enable-libcurl CFLAGS="-I$PREFIX/include" LDFLAGS="$LDFLAGS"
-make
-cd ..
-# Problem with ncurses from default channel we now get in bioconda so skip tview
 # https://github.com/samtools/samtools/issues/577
 if [[ "$(uname)" == "Linux" ]] ; then
     export LDFLAGS="$LDFLAGS -Wl,--add-needed"
 fi
-./configure CPPFLAGS="$CPPFLAGS" --prefix=$PREFIX --enable-libcurl --enable-plugins --with-plugin-path=$PWD/htslib-$PKG_VERSION LDFLAGS="$LDFLAGS" || (cat config.log ; false)
-make install prefix=$PREFIX LIBS+=-lcrypto LIBS+=-lcurl
+
+./configure --prefix=$PREFIX --enable-libcurl
+make
+make install
